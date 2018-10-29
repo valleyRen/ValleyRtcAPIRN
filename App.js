b2f11ccd72eaf8139240323eb06764dc01d10e0d
zhangjianping <114695092@qq.com>
2018-10-29 20:35:33 +0800

    rn-ios demo ok

--- App.js
+++ App.js
@@ -6,33 +6,44 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, NativeModules, NativeEventEmitter} from 'react-native';
+import React,{Component} from 'react'
+import {
+  Platform,
+	StyleSheet,
+	Text,
+	View,
+	TouchableHighlight,
+	NativeModules,
+	NativeEventEmitter
+} from 'react-native'
 
-/*
 import RNValleyRtcAPI from 'react-native-valley-rtc-api';
 
-RNValleyRtcAPI.addEvent("Birthday Party", "4 Privet Drive. Surrey");
+//RNValleyRtcAPI.addEvent("Birthday Party", "4 Privet Drive. Surrey");
+//
+//RNValleyRtcAPI.findEvents((error, events) => {
+//  if (error) {
+//    console.error(error);
+//  }
+//  else {
+//    console.log(events);
+//  }
+//});
+//
+//RNValleyRtcAPI.getIpAddress().then(ip => {
+//  // "92.168.32.44"
+//  console.log(ip);
+//});
 
-RNValleyRtcAPI.findEvents((error, events) => {
-  if (error) {
-    console.error(error);
-  }
-  else {
-    console.log(events);
-  }
-});
+//var emitter = new NativeEventEmitter(RNValleyRtcAPI);
+//subScription = emitter.addListener("ValleyCallback",(body) => {
+//  //console.error(body);
+//  //console.error(body.code);
+//  console.log(body.name);
+//});
+//subScription.remove();
 
-RNValleyRtcAPI.getIpAddress().then(ip => {
-  // "92.168.32.44"
-  console.log(ip);
-});
-
-var emitter = new NativeEventEmitter(RNValleyRtcAPI);
-subScription = emitter.addListener("geolocationDidChange",(body) => {console.log(body)});
-
-console.log(RNValleyRtcAPI.ERR_NOT_LOGINED);
-*/
+//console.log(RNValleyRtcAPI.ERR_NOT_LOGINED);
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -43,19 +54,199 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
+
+	state = {
+		text:'请点击',
+		notice:'未收到通知'
+	}
+
+	_getNotice (body) {
+		this.setState({
+			notice:body.userid + ',' + body.code
+		})
+	}
+	componentWillMount() {
+		//开始监听
+		var emitter = new NativeEventEmitter(RNValleyRtcAPI)
+		this.subScription = emitter.addListener("ValleyCallback",(body) => this._getNotice(body))
+	}
+	componentWillUnmount() {
+		//删除监听
+		this.subScription.remove()
+	}
+
+  _initSDK() {
+    RNValleyRtcAPI.InitSDK();
+    //RNValleyRtcAPI.SetAuthoKey('5b001900bc0366fcEnnQxsE');
+  }
+
+ 	async _createRoom() { //Promise回调
+    try {
+			var resolve = await RNValleyRtcAPI.CreateChannel(false)
+      RNValleyRtcAPI.ChannelEnableInterface(RNValleyRtcAPI.IID_RTCMSGR, (error) => {
+        if (error != RNValleyRtcAPI.ERR_SUCCEED) {
+          console.error(error);
+         }
+       });
+      this.setState({text:'create success'})
+    } catch(e) {
+          this.setState({
+            text:'create failed'
+          })
+    }
+	}
+  
+  _destoryRoom() {
+    RNValleyRtcAPI.ChannelRelease()
+  }
+  
+  _releaseSDK() {
+    RNValleyRtcAPI.CleanSDK();
+  }
+  
+  _login() {
+    var channelid = '5';
+    var userid = 'rn-guest';
+    RNValleyRtcAPI.ChannelLogin(channelid, userid, (error) => {
+      console.log(error);
+        if (error != RNValleyRtcAPI.ERR_SUCCEED) {
+          console.error(error);
+         }
+       });
+    
+    //RNValleyRtcAPI.ChannelGetLoginStatus((error) => {
+    //  console.log(error);
+    //    if (error != RNValleyRtcAPI.ERR_SUCCEED) {
+    //      console.error(error);
+    //     }
+    //   })
+  }
+  
+  _logout() {
+    RNValleyRtcAPI.ChannelLogout()
+  }
+  
+  _sendMsg() {
+    RNValleyRtcAPI.ChannelSendMsgr(RNValleyRtcAPI.TYPE_CMD, 'nihao', 'dsadsa', '', (error) => {
+      console.log(error);
+        if (error != RNValleyRtcAPI.ERR_SUCCEED) {
+          console.error(error);
+         }
+       })
+  }
+
+
+// 	async _promise(age) { //Promise回调
+//		try{
+//			var iOSExport = NativeModules.iOSExport
+//			var resolve = await iOSExport.rnToiOSAge(age)
+//			this.setState({
+//				text:resolve
+//			})
+//		}catch(e) {
+//			console.error(e);
+//		}
+//	}
+	//render() {
+	//	return(
+	//		<View>
+	//			<TouchableHighlight 
+	//				style={[styles.highLight,{marginTop:50}]} 
+	//				underlayColor='#deb887' 
+	//				activeOpacity={0.8}
+	//				onPress={() => this._initSDK()}
+	//				>
+	//				<Text>初始化</Text>
+	//			</TouchableHighlight>
+	//			<TouchableHighlight 
+	//				style={styles.highLight} 
+	//				underlayColor='coral' 
+	//				activeOpacity={0.8}
+	//				onPress={() => this._createRoom()}
+	//				>
+	//				<Text>创建房间</Text>
+	//			</TouchableHighlight>
+	//			<TouchableHighlight 
+	//				style={styles.highLight} 
+	//				underlayColor='#5f9ea0' 
+	//				activeOpacity={0.8}
+	//				onPress={() => this._createRoom()}
+	//				>
+	//				<Text>无用</Text>
+	//			</TouchableHighlight>
+	//			<TouchableHighlight 
+	//				style={styles.highLight} 
+	//				underlayColor='#5f9ea0' 
+	//				activeOpacity={0.8}
+	//				onPress={() => this._createRoom()}
+	//				>
+	//				<Text>无用</Text>
+	//			</TouchableHighlight>
+	//			<Text>{this.state.text}</Text>
+	//			<Text>{this.state.notice}</Text>
+	//		</View>
+	//	)
+	//}
+    render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native! LanPZzzz</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableHighlight 
+					style={[styles.highLight,{marginTop:50}]} 
+					underlayColor='#deb887' 
+					activeOpacity={0.8}
+					onPress={() => this._initSDK()}
+					>
+					<Text>初始化</Text>
+				</TouchableHighlight>
+        <TouchableHighlight 
+					style={[styles.highLight,{marginTop:50}]} 
+					underlayColor='#deb887' 
+					activeOpacity={0.8}
+					onPress={() => this._createRoom()}
+					>
+					<Text>创建房间</Text>
+				</TouchableHighlight>
+        <TouchableHighlight 
+					style={[styles.highLight,{marginTop:50}]} 
+					underlayColor='#deb887' 
+					activeOpacity={0.8}
+					onPress={() => this._login()}
+					>
+					<Text>进入房间</Text>
+				</TouchableHighlight>
+        <TouchableHighlight 
+					style={[styles.highLight,{marginTop:50}]} 
+					underlayColor='#deb887' 
+					activeOpacity={0.8}
+					onPress={() => this._logout()}
+					>
+					<Text>退出房间</Text>
+				</TouchableHighlight>
+        <TouchableHighlight 
+					style={[styles.highLight,{marginTop:50}]} 
+					underlayColor='#deb887' 
+					activeOpacity={0.8}
+					onPress={() => this._sendMsg()}
+					>
+					<Text>发送消息</Text>
+				</TouchableHighlight>
+        <Text style={styles.welcome}>state:{this.state.text}</Text>
+        <Text style={styles.welcome}>notice:{this.state.notice}</Text>
       </View>
     );
   }
 }
 
+//<Text style={styles.welcome}>
+//          state:{this.state.text}
+//        </Text>
+//        <Text style={styles.welcome}>
+//          notify:{this.notify.text}
+//        </Text>
+
+
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -71,4 +262,13 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+	highLight:{
+		height:60,
+		width:120,
+		margin:20,
+		padding:5,
+		borderWidth:1,
+		borderColor:'coral',
+		padding:2
+	}
+})
